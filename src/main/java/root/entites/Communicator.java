a52d13da195be55055653de28664339abb2bdc2e
= <=>
2021-02-18 22:16:11 +0700

    removing unused packages

--- src/main/java/root/entites/Communicator.java
+++ src/main/java/root/entites/Communicator.java
@@ -18,7 +18,6 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
-import org.hibernate.annotations.Fetch;
 import org.springframework.data.domain.Persistable;
 
 import root.password_encoder.PasswordEncoder;

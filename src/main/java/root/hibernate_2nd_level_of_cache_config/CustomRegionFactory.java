cc4f38e1649a40b09ba5c75c013b6ac47a51b5ca
= <=>
2021-02-18 13:58:28 +0700

    fixing bugs

--- src/main/java/root/hibernate_2nd_level_of_cache_config/CustomRegionFactory.java
+++ src/main/java/root/hibernate_2nd_level_of_cache_config/CustomRegionFactory.java
@@ -10,25 +10,14 @@ import org.redisson.hibernate.RedissonRegionFactory;
 public class CustomRegionFactory extends RedissonRegionFactory {
 	private static final long serialVersionUID = 1L;
 
-//	@Override
-//	protected RedissonClient createRedissonClient(Map properties) {
-//		String address = System.getenv().get("REDIS_URL");
-//        String[] passwordComponent1 = address.split(":");
-//        String[] passwordComponent2 = passwordComponent1[2].split("@");
-//        String password = passwordComponent2[0];
-//        Config config = new Config();
-//        config.useSingleServer()
-//       		  .setConnectionMinimumIdleSize(5)
-//       		  .setAddress(address)
-//       		  .setPassword(password);
-//        return Redisson.create(config);
-//	}
-	
 	@Override
 	protected RedissonClient createRedissonClient(Map properties) {
-		String address = "redis://:p930a4db3e26eee29e3b382a81f7daaa87adf58b71be3779a487f4f0abf2b41eb@ec2-34-198-87-71.compute-1.amazonaws.com:21979";
+		String address = System.getenv().get("REDIS_URL");
+		if(address == null) {
+			address = "redis://:p930a4db3e26eee29e3b382a81f7daaa87adf58b71be3779a487f4f0abf2b41eb@ec2-34-198-87-71.compute-1.amazonaws.com:21979";
+		}
         String[] passwordComponent1 = address.split(":");
-        String[] passwordComponent2 =passwordComponent1[2].split("@");
+        String[] passwordComponent2 = passwordComponent1[2].split("@");
         String password = passwordComponent2[0];
         Config config = new Config();
         config.useSingleServer()
@@ -37,6 +26,20 @@ public class CustomRegionFactory extends RedissonRegionFactory {
        		  .setPassword(password);
         return Redisson.create(config);
 	}
+	
+//	@Override
+//	protected RedissonClient createRedissonClient(Map properties) {
+//		String address = "redis://:p930a4db3e26eee29e3b382a81f7daaa87adf58b71be3779a487f4f0abf2b41eb@ec2-34-198-87-71.compute-1.amazonaws.com:21979";
+//        String[] passwordComponent1 = address.split(":");
+//        String[] passwordComponent2 =passwordComponent1[2].split("@");
+//        String password = passwordComponent2[0];
+//        Config config = new Config();
+//        config.useSingleServer()
+//       		  .setConnectionMinimumIdleSize(5)
+//       		  .setAddress(address)
+//       		  .setPassword(password);
+//        return Redisson.create(config);
+//	}
 	
 	
 
--- src/main/java/root/session_config/SessionConfig.java
+++ src/main/java/root/session_config/SessionConfig.java
@@ -18,28 +18,31 @@ public class SessionConfig extends AbstractHttpSessionApplicationInitializer {
         return new RedissonConnectionFactory(redisson);
     }
 
-//    @Bean(destroyMethod = "shutdown")
-//    public RedissonClient redisson() {
-//    	String address = System.getenv().get("REDIS_URL");
-//        String[] passwordComponent1 = address.split(":");
-//        String[] passwordComponent2 = passwordComponent1[2].split("@");
-//        String password = passwordComponent2[0];
-//        Config config = new Config();
-//        config.useSingleServer()
-//       		  .setConnectionMinimumIdleSize(10)
-//       		  .setAddress(address)
-//       		  .setPassword(password);
-//        return Redisson.create(config);
-//    }
-    
     @Bean(destroyMethod = "shutdown")
     public RedissonClient redisson() {
+    	String address = System.getenv().get("REDIS_URL");
+    	if(address == null) {
+    		address = "redis://:p930a4db3e26eee29e3b382a81f7daaa87adf58b71be3779a487f4f0abf2b41eb@ec2-34-198-87-71.compute-1.amazonaws.com:21979";
+    	}
+        String[] passwordComponent1 = address.split(":");
+        String[] passwordComponent2 = passwordComponent1[2].split("@");
+        String password = passwordComponent2[0];
         Config config = new Config();
         config.useSingleServer()
        		  .setConnectionMinimumIdleSize(10)
-       		  .setAddress("redis://:p930a4db3e26eee29e3b382a81f7daaa87adf58b71be3779a487f4f0abf2b41eb@ec2-34-198-87-71.compute-1.amazonaws.com:21979")
-       		  .setPassword("p930a4db3e26eee29e3b382a81f7daaa87adf58b71be3779a487f4f0abf2b41eb");
+       		  .setAddress(address)
+       		  .setPassword(password);
         return Redisson.create(config);
     }
+    
+//    @Bean(destroyMethod = "shutdown")
+//    public RedissonClient redisson() {
+//        Config config = new Config();
+//        config.useSingleServer()
+//       		  .setConnectionMinimumIdleSize(10)
+//       		  .setAddress("redis://:p930a4db3e26eee29e3b382a81f7daaa87adf58b71be3779a487f4f0abf2b41eb@ec2-34-198-87-71.compute-1.amazonaws.com:21979")
+//       		  .setPassword("p930a4db3e26eee29e3b382a81f7daaa87adf58b71be3779a487f4f0abf2b41eb");
+//        return Redisson.create(config);
+//    }
 
 }
